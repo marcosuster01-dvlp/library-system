bc8bb7d3fbf66547d7ad67939e22397c0dcd4c90
Marcos <marcosuster01@gmail.com>
2026-05-20 13:38:34 +0000

    feat: add get_available_books function

--- models/library.py
+++ models/library.py
@@ -6,4 +6,7 @@ class Library:
     def add_book(self,book):
         self.books.append(book)
         return self.books
+    
+    def get_available_books(self):
+        return [book for book in self.books if book.available]
         
