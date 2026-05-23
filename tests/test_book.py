5a1afcae1d8b503423ce6d5786c2ccbc65095249
Marcos <marcosuster01@gmail.com>
2026-05-23 21:45:39 +0000

    test: add checkout and checkout not avaiable tests

--- tests/test_book.py
+++ tests/test_book.py
@@ -5,3 +5,15 @@ def test_get_info():
     info = bk.get_info()
     assert "The Hobbit" in info
     assert "R.R. Tolkien" in info
+
+def test_checkout():
+    bk = Book("The Hobbit", "R.R. Tolkien", 2010)
+    bk.checkout()
+    assert bk.is_available == False
+
+def test_checkout_not_available():
+    bk = Book("The Hobbit", "R.R. Tolkien", 2010)
+    bk.checkout()
+
+    not_available = bk.checkout()
+    assert not_available == "Book not available"
